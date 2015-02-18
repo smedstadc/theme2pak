ace9db95d3ace90132f525d2c01dc6332647d13e
Corey Smedstad <smedstadc@gmail.com>
2015-02-18 01:51:15 -0500

    convert for proper command line usage, improve code readability, add requirements.txt

--- pidgintheme2phpbbpak.py
+++ pidgintheme2phpbbpak.py
@@ -6,92 +6,90 @@ theme file must be named "theme"
 outputs "converted.pak" and "log.txt"
 
 upload converted.pak and matching images to your <phpbb root>/images/smilies/
-then install the smiley through the forum softwares admin panel
-keep in mind that phpbb3 places a hard limit of 1000 on installed smilies
-you can mod this 
+then install the smiley through the forum admin control panel keep in mind
+that phpbb3 places a hard limit of 1000 on installed smilies by default you
+can mod this if you need to.
 
 Only preserves the first emote code as phpbb doesn't support multiple per image.
-In the future I'll implement an interactive mode that asks which to keep.
+Maybe someday I'll implement an interactive mode that asks which to keep.
 """
 
 
 # TODO:
-# 1. Clean up for command line use.
-# 2. Interactive mode.
+# 1. Interactive mode ???
 #
 # Reminders:
 # pak line format is 
-# 'thumbsup3d.gif', '15', '15', '1', 'Thumbs Up', ':yes:',
 # filename, width, height, visible, name, emote code
+# 'thumbsup3d.gif', '15', '15', '0', 'Thumbs Up', ':yes:',
+# the visible attribute controls whether a smiley is visible on the posting page 1=yes, 0=no
+# you most likely don't want this to be 1 for the bulk of them because it will make post forms
+# scroll too much vertically
 # PIL.Image.size is a (width, height) tuple
 
-__author__ = "Corey Smedstad"
-
+from __future__ import print_function
 from PIL import Image
 import os
-import sys
+import argh
+import logging
 
-# controls whether a smiley is visible on the posting page 1=yes, 0=no
-# you most likely don't want this to be 1 for the bulk of them
-visible = '0'
 
-def quoted(s):
-    """
-    Returns a quoted, comma delimited string.
-    For example "s" becomes "'s', "
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('theme2pak')
 
-    s: string to surround with quotes.
-    """
-    leftquote = '\''
-    rightquote = '\', '
-    return leftquote + s + rightquote
 
-cwd = os.path.dirname(__file__)
-filename = os.path.join(cwd, 'theme')
-themefile = open(filename, 'r')
-count = 0
-skipped = 0
+def convert_theme(input_path, output_path, verbose=False):
+    if verbose:
+        logger.setLevel(logging.DEBUG)
 
-try:
-    outputfile = open(os.path.join(cwd, 'converted.pak'), 'w')
-except IOError:
-    print "Error opening output file. Quitting."
-    sys.exit()
+    logger.info("Converting {} -> {}".format(input_path, output_path))
+    logger.debug("in convert_theme with input_path={}, output_path={}".format(repr(input_path), repr(output_path)))
+    input_path = os.path.join(os.getcwd(), input_path)
+    output_path = os.path.join(os.getcwd(), output_path)
+    base_path = os.path.split(input_path)[:-1][0]
 
-try:
-    logfile = open(os.path.join(cwd, 'log.txt'), 'w')
-except IOError:
-    print "Error opening log file. Quitting."
-    sys.exit()
+    try:
+        with open(input_path, 'r') as theme_file, open(output_path, 'w') as output_file:
+            for line in theme_file:
+                logger.debug("Read: {}".format(repr(line)))
+                if line:
+                    pak_line = get_pak_line_from_theme_line(line, base_path)
+                    if pak_line:
+                        logger.debug("Output: {}".format(repr(pak_line)))
+                        output_file.write(pak_line)
+                        print('.', end='')
+            print('\nDone!\n')
+    except Exception as e:
+        logger.error(e)
 
-logfile.write("Script started...\n")
-logfile.write("-" * 79 + "\n")
 
-for line in themefile:
-    if line[0] == '!':
-        s = line.split()
-        emotefile = s[1]
-        emotecode = s[2]
-        try:
-            emoteimage = Image.open(os.path.join(cwd, emotefile))
-            emotewidth = emoteimage.size[0]
-            emoteheight = emoteimage.size[1]
-            outputfile.write(quoted(emotefile))
-            outputfile.write(quoted(str(emotewidth)))
-            outputfile.write(quoted(str(emoteheight)))
-            outputfile.write(quoted(str(visible)))
-            outputfile.write(quoted(emotefile.split('.')[0]))
-            outputfile.write(quoted(emotecode))
-            outputfile.write('\n')
-            count += 1
-        except IOError:
-            print "Skipping " + str(emotecode) + " for missing file."
-            logfile.write("Could not find file: " + emotefile + " for " + emotecode + "\n")
-            skipped += 1
+def get_pak_line_from_theme_line(line, base_path):
+    logger.debug("in get_pak_line_from_theme_line with line={}, base_path={}".format(repr(line), repr(base_path)))
 
-logfile.write("-" * 79 + "\n")
-logfile.write("Added " + str(count) + " smilies to converted.pak\n")
-logfile.write("Skipped " + str(skipped) + " missing files.\n")
-logfile.write("Script finished...\n")
-outputfile.close()
-logfile.close()
+    if line is not None and line[0] == '!':
+        emote_filename = line.split()[1]
+        emote_bbcode = line.split()[2]
+        emote_size = get_emote_image_dimensions(os.path.join(base_path, emote_filename))
+
+        if emote_size:
+            emote_width, emote_height = get_emote_image_dimensions(os.path.join(base_path, emote_filename))
+            return "'{0}', '{1}', '{2}', '0', '{3}', '{4}',\n".format(emote_filename, emote_width,
+                                                                      emote_height, emote_filename.split('.')[0],
+                                                                      emote_bbcode)
+        else:
+            return None
+
+
+def get_emote_image_dimensions(path):
+    logger.debug("in get_emote_image_dimensions with path={}".format(repr(path)))
+    try:
+        with Image.open(path) as emote_image:
+            return emote_image.size
+    except IOError as e:
+        logger.error("Problem reading image {}".format(repr(path)))
+    except Exception as e:
+        logger.error(e)
+
+
+if __name__ == "__main__":
+    argh.dispatch_command(convert_theme)
